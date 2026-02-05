5070db99aa47db619a2daa5fe9c0d4736f8ee8a6
maciula2 <macixxo@gmail.com>
2026-02-05 13:47:42 +0100

    removed adding break symbol for last line

--- fade/__init__.py
+++ fade/__init__.py
@@ -4,8 +4,9 @@ from os import system
 
 def blackwhite(text, horizontal=False):
     system(""); faded = ""
+    lines = text.splitlines()
     if horizontal:
-        for line in text.splitlines():
+        for i, line in enumerate(lines):
             width = len(line)
             for i, char in enumerate(line):
                 if width > 1:
@@ -16,11 +17,15 @@ def blackwhite(text, horizontal=False):
                 green = int(180 *(1-t))
                 blue = int(180 *(1-t))
                 faded += f"\033[38;2;{red};{green};{blue}m{char}\033[0m"
-            faded += "\n"
+            if line != lines[len(lines)-1]:
+                faded += "\n"
         return faded
     red = 0; green = 0; blue = 0
-    for line in text.splitlines():
-        faded += (f"\033[38;2;{red};{green};{blue}m{line}\033[0m\n")
+    for i, line in enumerate(lines):
+        if line != lines[len(lines)-1]:
+            faded += (f"\033[38;2;{red};{green};{blue}m{line}\033[0m\n")
+        else:
+            faded += (f"\033[38;2;{red};{green};{blue}m{line}\033[0m")
         if not red == 255 and not green == 255 and not blue == 255:
             red += 20; green += 20; blue += 20
             if red > 255 and green > 255 and blue > 255:
@@ -29,8 +34,9 @@ def blackwhite(text, horizontal=False):
 
 def purplepink(text, horizontal=False):
     system(""); faded = ""
+    lines = text.splitlines()
     if horizontal:
-        for line in text.splitlines():
+        for i, line in enumerate(lines):
             width = len(line)
             for i, char in enumerate(line):
                 if width > 1:
@@ -39,12 +45,17 @@ def purplepink(text, horizontal=False):
                     t=0
                 red = int(195 *(1-t))
                 faded += f"\033[38;2;{red};0;220m{char}\033[0m"
-            faded += "\n"
+            if line != lines[len(lines)-1]:
+                faded += "\n"
         return faded
     system(""); faded = ""
     red = 40
-    for line in text.splitlines():
-        faded += (f"\033[38;2;{red};0;220m{line}\033[0m\n")
+    for i, line in enumerate(lines):
+        if line != lines[len(lines)-1]:
+            faded += (f"\033[38;2;{red};0;220m{line}\033[0m\n")
+        else:
+            faded += (f"\033[38;2;{red};0;220m{line}\033[0m")
+
         if not red == 255:
             red += 15
             if red > 255:
@@ -53,8 +64,9 @@ def purplepink(text, horizontal=False):
 
 def greenblue(text, horizontal=False):
     system(""); faded = ""
+    lines = text.splitlines()
     if horizontal:
-        for line in text.splitlines():
+        for i, line in enumerate(lines):
             width = len(line)
             for i, char in enumerate(line):
                 if width > 1:
@@ -63,11 +75,15 @@ def greenblue(text, horizontal=False):
                     t=0
                 blue = int(235 *(1-t))
                 faded += f"\033[38;2;0;255;{blue}m{char}\033[0m"
-            faded += "\n"
+            if line != lines[len(lines)-1]:
+                faded += "\n"
         return faded
     blue = 100
-    for line in text.splitlines():
-        faded += (f"\033[38;2;0;255;{blue}m{line}\033[0m\n")
+    for i, line in enumerate(lines):
+        if line != lines[len(lines)-1]:
+            faded += (f"\033[38;2;0;255;{blue}m{line}\033[0m\n")
+        else:
+            faded += (f"\033[38;2;0;255;{blue}m{line}\033[0m")
         if not blue == 255:
             blue += 15
             if blue > 255:
@@ -76,8 +92,9 @@ def greenblue(text, horizontal=False):
 
 def pinkred(text, horizontal=False):
     system(""); faded = ""
+    lines = text.splitlines()
     if horizontal:
-        for line in text.splitlines():
+        for i, line in enumerate(lines):
             width = len(line)
             for i, char in enumerate(line):
                 if width > 1:
@@ -86,11 +103,15 @@ def pinkred(text, horizontal=False):
                     t=0
                 blue = int(250 *(1-t))
                 faded += f"\033[38;2;255;0;{blue}m{char}\033[0m"
-            faded += "\n"
+            if line != lines[len(lines)-1]:
+                faded += "\n"
         return faded
     blue = 255
-    for line in text.splitlines():
-        faded += (f"\033[38;2;255;0;{blue}m{line}\033[0m\n")
+    for i, line in enumerate(lines):
+        if line != lines[len(lines)-1]:
+            faded += (f"\033[38;2;255;0;{blue}m{line}\033[0m\n")
+        else:
+            faded += (f"\033[38;2;255;0;{blue}m{line}\033[0m")
         if not blue == 0:
             blue -= 20
             if blue < 0:
@@ -99,8 +120,9 @@ def pinkred(text, horizontal=False):
 
 def purpleblue(text, horizontal=False):
     system(""); faded = ""
+    lines = text.splitlines()
     if horizontal:
-        for line in text.splitlines():
+        for i, line in enumerate(lines):
             width = len(line)
             for i, char in enumerate(line):
                 if width > 1:
@@ -109,12 +131,15 @@ def purpleblue(text, horizontal=False):
                     t=0
                 red = int(110 *(1-t))
                 faded += f"\033[38;2;{red};0;255m{char}\033[0m"
-
-            faded += "\n"
+            if line != lines[len(lines)-1]:
+                faded += "\n"
         return faded
     red = 110
-    for line in text.splitlines():
-        faded += (f"\033[38;2;{red};0;255m{line}\033[0m\n")
+    for i, line in enumerate(lines):
+        if line != lines[len(lines)-1]:
+            faded += (f"\033[38;2;{red};0;255m{line}\033[0m\n")
+        else:
+            faded += (f"\033[38;2;{red};0;255m{line}\033[0m")
         if not red == 0:
             red -= 15
             if red < 0:
@@ -123,8 +148,9 @@ def purpleblue(text, horizontal=False):
 
 def water(text, horizontal=False):
     system(""); faded = ""
+    lines = text.splitlines()
     if horizontal:
-        for line in text.splitlines():
+        for i, line in enumerate(lines):
             width = len(line)
             for i, char in enumerate(line):
                 if width > 1:
@@ -133,11 +159,15 @@ def water(text, horizontal=False):
                     t=0
                 green = int(200 *(1-t))
                 faded += f"\033[38;2;0;{green};255m{char}\033[0m"
-            faded += "\n"
+            if line != lines[len(lines)-1]:
+                faded += "\n"
         return faded
     green = 10
-    for line in text.splitlines():
-        faded += (f"\033[38;2;0;{green};255m{line}\033[0m\n")
+    for i, line in enumerate(lines):
+        if line != lines[len(lines)-1]:
+            faded += (f"\033[38;2;0;{green};255m{line}\033[0m\n")
+        else:
+            faded += (f"\033[38;2;0;{green};255m{line}\033[0m")
         if not green == 255:
             green += 15
             if green > 255:
@@ -147,8 +177,9 @@ def water(text, horizontal=False):
 
 def fire(text, horizontal=False):
     system(""); faded = ""
+    lines = text.splitlines()
     if horizontal:
-        for line in text.splitlines():
+        for i, line in enumerate(lines):
             width = len(line)
             for i, char in enumerate(line):
                 if width > 1:
@@ -157,11 +188,15 @@ def fire(text, horizontal=False):
                     t=0
                 green = int(250 *(1-t))
                 faded += f"\033[38;2;255;{green};0m{char}\033[0m"
-            faded += "\n"
+            if line != lines[len(lines)-1]:
+                faded += "\n"
         return faded
     green = 250    
-    for line in text.splitlines():
-        faded += (f"\033[38;2;255;{green};0m{line}\033[0m\n")
+    for i, line in enumerate(lines):
+        if line != lines[len(lines)-1]:
+            faded += (f"\033[38;2;255;{green};0m{line}\033[0m\n")
+        else:
+            faded += (f"\033[38;2;255;{green};0m{line}\033[0m")
         if not green == 0:
             green -= 25
             if green < 0:
@@ -170,8 +205,9 @@ def fire(text, horizontal=False):
 
 def brazil(text, horizontal=False):
     system(""); faded = ""
+    lines = text.splitlines()
     if horizontal:
-        for line in text.splitlines():
+        for i, line in enumerate(lines):
             width = len(line)
             for i, char in enumerate(line):
                 if width > 1:
@@ -180,19 +216,25 @@ def brazil(text, horizontal=False):
                     t=0
                 red = int(250 *(1-t))
                 faded += f"\033[38;2;{red};255;0m{char}\033[0m"
-            faded += "\n"
+            if line != lines[len(lines)-1]:
+                faded += "\n"
         return faded
     red = 0
-    for line in text.splitlines():
-        faded += (f"\033[38;2;{red};255;0m{line}\033[0m\n")
+    for i, line in enumerate(lines):
+        if line != lines[len(lines)-1]:
+            faded += (f"\033[38;2;{red};255;0m{line}\033[0m\n")
+        else:
+            faded += (f"\033[38;2;{red};255;0m{line}\033[0m")
         if not red > 200:
             red += 30
     return faded
 
 def random(text):
     system(""); faded = ""
-    for line in text.splitlines():
+    lines = text.splitlines()
+    for i, line in enumerate(lines):
         for character in line:
             faded += (f"\033[38;2;{randint(0,255)};{randint(0,255)};{randint(0,255)}m{character}\033[0m")
-        faded += "\n"
+        if line != lines[len(lines)-1]:
+                faded += "\n"
     return faded
